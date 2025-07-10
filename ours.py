a0cc0efbec1b319caca3d2f4abcab81d63f65658
weathon <wguo6358@gmail.com>
2025-07-09 19:15:30 -0700

    give up

--- ours.py
+++ ours.py
@@ -1,63 +1,10 @@
 import torch
 from processor import JointAttnProcessor2_0
+from torch.nn.attention.flex_attention import create_block_mask
+import numpy as np
+
+
 def inference(pipe, prompt, neg_prompt, seed=0, scale=3):
-    # (
-    #     pos_prompt_embeds,
-    #     _,
-    #     pos_pooled_prompt_embeds,
-    #     _,
-    # ) = pipe.encode_prompt(
-    #     prompt=prompt,
-    #     prompt_2=prompt,
-    #     prompt_3=prompt,
-    #     max_sequence_length = 77
-    # )
-
-    # (
-    #     neg_prompt_embeds,
-    #     _,
-    #     neg_pooled_prompt_embeds,
-    #     _,
-    # ) = pipe.encode_prompt(
-    #     prompt=neg_prompt,
-    #     prompt_2=neg_prompt,
-    #     prompt_3=neg_prompt,
-    #     max_sequence_length = 77
-    # )
-
-    # negative_prompt_length = [len(pipe.tokenizer(neg_prompt).input_ids), len(pipe.tokenizer_3(neg_prompt).input_ids)]
-    # attn_mask = torch.ones((1, 4404, 4404)).bool()
-    # # attn_mask[:,-154*2:,-154*2:] = False #text cannot see each other 
-    # # attn_mask[:,-154*2:-154,-154*2:-154] = True # positive prompt can see each other   
-    # attn_mask[:,-154:,154:] = False  #missing negative sign
-    # # attn_mask[:,-154*2:,-154*2:] = False
-    # # attn_mask[:,-154*2:-154,-154*2:-154] = True
-    
-    # attn_mask[:,-154+negative_prompt_length[0]:-77:,:] = False
-    # attn_mask[:,:,-154+negative_prompt_length[0]:-77] = False
-    # attn_mask[:,-77+negative_prompt_length[1]:,:] = False
-    # attn_mask[:,:,-77+negative_prompt_length[1]:] = False
-    
-    # # attn_mask[:,-154*2+negative_prompt_length[0]:-154*2+77:,:] = False
-    # # attn_mask[:,:,-154*2+negative_prompt_length[0]:-154*2] = False
-    # # attn_mask[:,-154-77+negative_prompt_length[1]:-154,:] = False
-    # # attn_mask[:,:,-154-77+negative_prompt_length[1]:-154] = False
-    # attn_mask = attn_mask.cuda()
-
-    # for block in pipe.transformer.transformer_blocks:
-    #     block.attn.processor = JointAttnProcessor2_0(scale=scale, attn_mask=attn_mask, neg_prompt_length=negative_prompt_length)
-
-    # prompt_embeds = torch.cat([pos_prompt_embeds, neg_prompt_embeds], dim=1)
-    # image_ours = pipe(
-    #     prompt_embeds=prompt_embeds,
-    #     pooled_prompt_embeds=pos_pooled_prompt_embeds,
-    #     num_inference_steps=8,
-    #     guidance_scale=0.0,
-    #     nag_scale=0.0,
-    #     generator=torch.manual_seed(seed),
-    # ).images[0]  
-
-    # return image_ours
     (
         pos_prompt_embeds,
         _,
@@ -84,26 +31,25 @@ def inference(pipe, prompt, neg_prompt, seed=0, scale=3):
     pos_len = pos_prompt_embeds.shape[1]
     
     prompt_embeds = torch.cat([pos_prompt_embeds, neg_prompt_embeds], dim=1)
-    attn_mask = torch.zeros((1, 4096 + prompt_embeds.shape[1], 4096 + prompt_embeds.shape[1] + neg_len))
+    attn_mask = np.ones((1, 4096 + prompt_embeds.shape[1], prompt_embeds.shape[1] + neg_len))
+    attn_mask[:,-neg_len-pos_prompt_embeds.shape[1]:,-neg_len:] = False
+    attn_mask[:,:-neg_len,-2*neg_len:-neg_len] = False
+    attn_mask[:,-neg_len:,:pos_prompt_embeds.shape[1]] = False
+    def mask_mod(b, h, q_idx, kv_idx):
+        if kv_idx < 4096:
+            return True
+        return attn_mask[0, q_idx, kv_idx-4096].bool()
+        
     
-    attn_mask[:,-neg_len-pos_prompt_embeds.shape[1]:,-neg_len:] = -torch.inf
-    attn_mask[:,:-neg_len,-2*neg_len:-neg_len] = -torch.inf
-    attn_mask[:,-neg_len:,4096+pos_prompt_embeds.shape[1]] = -torch.inf
-    
-    def score_mod(score, b, h, q_idx, kv_idx):
-        mask = torch.logical_and(q_idx > 4096, kv_idx > (4096 + pos_len + neg_len)) # bottom right
-        mask = torch.logical_or(mask, torch.logical_and(q_idx < (4096 + pos_len), (kv_idx > (4096 + pos_len)).logical_and(kv_idx < (4096 + pos_len + neg_len)))) # left
-        mask = torch.logical_or(mask, torch.logical_and(q_idx > (4096 + pos_len), (kv_idx > 4096).logical_and(kv_idx < (4096 + pos_len)))) # top right
-        return torch.where(mask, score, -torch.inf)
-
-    for block in pipe.transformer.transformer_blocks:
-        block.attn.processor = JointAttnProcessor2_0(scale=scale, neg_prompt_length=neg_len, score_mod=score_mod)
+    block_mask = create_block_mask(mask_mod, B=None, H=None, Q_LEN=4096+pos_len+neg_len, KV_LEN=4096+pos_len+neg_len+neg_len)
+    for block in pipe.transformer.transformer_blocks: 
+        block.attn.processor = JointAttnProcessor2_0(scale=scale, neg_prompt_length=neg_len, score_mod=None, block_mask=block_mask)
 
     # pipe.transformer = torch.compile(pipe.transformer)
     image_ours = pipe(
         prompt_embeds=prompt_embeds,
         pooled_prompt_embeds=pos_pooled_prompt_embeds,
-        num_inference_steps=8,
+        num_inference_steps=8, 
         guidance_scale=0.0,
         nag_scale=0.0,
         generator=torch.manual_seed(seed),
--- processor.py
+++ processor.py
@@ -39,12 +39,13 @@ else:
 class JointAttnProcessor2_0:
     """Attention processor used typically in processing the SD3-like self-attention projections."""
 
-    def __init__(self, scale=4, score_mod=None, neg_prompt_length=0):
+    def __init__(self, scale=4, score_mod=None, block_mask=None, neg_prompt_length=0):
         if not hasattr(F, "scaled_dot_product_attention"):
             raise ImportError("JointAttnProcessor2_0 requires PyTorch 2.0, to use it, please upgrade PyTorch to 2.0.")
         self.score_mod = score_mod
         self.neg_prompt_length = neg_prompt_length
         self.scale = scale
+        self.block_mask = block_mask
 
     def __call__(
         self,
@@ -101,7 +102,7 @@ class JointAttnProcessor2_0:
             value[:,:,-self.neg_prompt_length:] *= -self.scale  
             
         # hidden_states = F.scaled_dot_product_attention(query, key, value, dropout_p=0.0, is_causal=False, attn_mask=self.attn_mask.to(query.dtype))
-        hidden_states = flex_attention(query, key, value, score_mod=self.score_mod)
+        hidden_states = flex_attention(query, key, value, score_mod=self.score_mod, block_mask=self.block_mask)
         hidden_states = hidden_states.transpose(1, 2).reshape(batch_size, -1, attn.heads * head_dim)
         hidden_states = hidden_states.to(query.dtype)
 
