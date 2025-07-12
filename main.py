used the wrong offset sign

main.py
@@ -73,7 +73,7 @@ async def judge_async(image_ours, image_nag, prompt, neg_prompt):
     })
     
 
-with open("prompts.json", "r") as f:
+with open("prompts2.json", "r") as f:
     prompts_data = json.load(f)
 
 prompts = pd.read_csv("sampled.csv")
@@ -100,7 +100,7 @@ for j in range(5):
         # neg_prompt = "low quality, blurry, bad lighting, poor detail"
         ours_starts = time.time()
         torch.cuda.reset_peak_memory_stats()
-        image_ours = inference(pipe, prompt, neg_prompt, seed=seed, scale=1, offset=-0.5)
+        image_ours = inference(pipe, prompt, neg_prompt, seed=seed, scale=4, offset=0.1)
         ours_time += time.time() - ours_starts
         ours_max_mem += torch.cuda.max_memory_allocated() / 1024 / 1024 / 1024
         

quality.py
@@ -54,8 +54,8 @@ run_baseline = False
 
 def run():
     wandb.init(project="VSF", config={
-        "scale": 0.4,
-        "offset": -0.15,
+        "scale": 0.2,
+        "offset": 0.1,
     })
     
     prompts = metadata_df["prompt"].sample(100)
@@ -71,7 +71,7 @@ def run():
             nag_negative_prompt=neg_prompt,
             generator=torch.manual_seed(seed),
             guidance_scale=0.,
-            nag_scale=5,
+            nag_scale=0,#5,
             num_inference_steps=8,
             nag_alpha=0.25,
             nag_tau=2.5